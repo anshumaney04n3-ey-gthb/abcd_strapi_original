e999be474c89f4416e2e09eafbf84be1be183cdd
anshuman-ranjan-ABCD-EY <anshuman.ranjan-v@adityabirlacapital.com>
2026-03-31 15:21:35 +0000

    update repo

--- shell-seed-scripts/config.sh
+++ shell-seed-scripts/config.sh
@@ -4,7 +4,7 @@
 # Uncomment the environment you want to use
 
 # LOCAL
-# BASE_URL="http://localhost:1337"
+BASE_URL="http://localhost:1337"
 
 # UAT
 BASE_URL="https://abcduat1.abcscuat.com/strapi"
